48e5fa9568524bdfd653be9bfe8b0335175cf16b
Oren Tirosh <orent@hishome.net>
2021-09-04 13:04:46 +0000

    more

--- parse.py
+++ parse.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from clang import cindex
 import os
+import subprocess
 
 class EasyIndex(cindex.Index):
     def __new__(cls, *args):
@@ -9,13 +10,13 @@ class EasyIndex(cindex.Index):
         return new
 
     def __init__(self, dbpath):
-        self.compdb = cindex.CompilationDatabase.fromDirectory(dbpath)
+        compdb = cindex.CompilationDatabase.fromDirectory(dbpath)
         self.compile_commands = {
             os.path.abspath(cc.filename) : dict(
                 directory=cc.directory,
                 arguments=list(cc.arguments),
             )
-            for cc in self.compdb.getAllCompileCommands()
+            for cc in compdb.getAllCompileCommands()
         }
         self.options_by_compiler = {}
 
@@ -29,13 +30,43 @@ class EasyIndex(cindex.Index):
             if a == '-o':
                 del arguments[i:i+2]
                 break
-        return arguments
+        return ['-working-directory='+cc['directory']] + self.getcompilerargs(compiler) + arguments
+        return self.getcompilerargs(compiler) + arguments
 
     def getcompilerargs(self, compiler):
-
-
+        try:
+            return self.options_by_compiler[compiler]
+        except KeyError:
+            pass
+        output = subprocess.check_output(
+                [compiler, '-E', '-v', '-'], 
+                stdin=open('/dev/null'),
+                stderr=subprocess.STDOUT,
+                encoding='latin1',
+        )
+        lines = output.splitlines()
+        a = lines.index('#include "..." search starts here:')
+        b = lines.index('#include <...> search starts here:')
+        c = lines.index('End of search list.')
+        inc = lines[a+1:b]
+        isystem = lines[b+1:c]
+        args = []
+        for path in inc:
+            args.append('-I')
+            args.append(path.strip())
+        for path in isystem:
+            args.append('-isystem')
+            args.append(path.strip())
+        self.options_by_compiler[compiler] = args
+        return args
 
     def parse(self, path, args=None, *moreargs, **kwargs):
-        args = self.getargs(path)
+        abspath = os.path.abspath(path)
+        args = self.getargs(abspath) + list(args or [])
         print(args)
-        return super(EasyIndex, self).parse(path, args, *moreargs, **kwargs)
+
+        cwd = os.getcwd()
+        try:
+            return super(EasyIndex, self).parse(abspath, args, *moreargs, **kwargs)
+        finally:
+            os.chdir(cwd)
